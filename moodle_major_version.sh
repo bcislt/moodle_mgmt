375231ca61b06177db1318929aa0b8b8003d6ea5
Conor Bradley <cbradley@borderscollege.ac.uk>
2019-08-13 08:54:45 +0100

    updated to moodle 3.7

--- moodle_major_version.sh
+++ moodle_major_version.sh
@@ -3,10 +3,10 @@ git clone https://github.com/bcislt/moodle moodle
 cd moodle
 git remote add upstream https://github.com/moodle/moodle
 git fetch upstream
-git branch --track MOODLE_36_STABLE upstream/MOODLE_36_STABLE
-git checkout MOODLE_36_STABLE
-git checkout -b BC_MOODLE_36_STABLE
+git branch --track MOODLE_37_STABLE upstream/MOODLE_37_STABLE
+git checkout MOODLE_37_STABLE
+git checkout -b BC_MOODLE_37_STABLE
 git add --all
-git commit -m 'New branch for Moodle 3.6'
-git push --set-upstream origin BC_MOODLE_36_STABLE
+git commit -m 'New branch for Moodle 3.7'
+git push --set-upstream origin BC_MOODLE_37_STABLE
 echo '########################## Now add your plugins etc. then do git add/commit/push ##########################' 
